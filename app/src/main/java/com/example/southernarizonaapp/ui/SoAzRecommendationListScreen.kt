26d277d039072a5d0908078c2832cd8eec9a60a5
jcisking <krisloeffler@gmail.com>
2023-03-15 00:12:41 -0700

    another commit

--- app/src/main/java/com/example/southernarizonaapp/ui/SoAzRecommendationListScreen.kt
+++ app/src/main/java/com/example/southernarizonaapp/ui/SoAzRecommendationListScreen.kt
@@ -6,9 +6,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -19,13 +17,18 @@ import androidx.compose.ui.graphics.Color
 import com.example.southernarizonaapp.data.Recommendation
 import com.example.southernarizonaapp.data.local.LocalRecommendationDataProvider
 import com.example.southernarizonaapp.ui.theme.SouthernArizonaAppTheme
+import com.example.southernarizonaapp.R
 
 @Composable
 fun SoAzRecommendationListScreen(
     recommendations: List<Recommendation>
 ) {
-    Scaffold() { it ->
-        LazyColumn(modifier = Modifier.padding(it).fillMaxWidth()) {
+    Scaffold(
+        topBar = { RecommendationListScreenTopBar() }
+    ) { it ->
+        LazyColumn(modifier = Modifier
+            .padding(it)
+            .fillMaxWidth()) {
             items(recommendations) {
                 RecommendationListItem(it)
             }
@@ -45,6 +48,20 @@ fun RecommendationListItem(
         }
     }
 
+}
+
+@Composable 
+fun RecommendationListScreenTopBar(
+    uiState: SoAzUiState,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(modifier = modifier) {
+        IconButton(onClick = { /*TODO*/ }) {
+            Icon(painter = , contentDescription = )
+            
+        }
+        Text(text = stringResource(id = R.string.recomm_list_screen_top_bar, uiState.currentCategory ))
+    }
 }
 
 
