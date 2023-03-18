8088dcaf65301869ad4ce9c12f67e3952ecc0be2
jcisking <krisloeffler@gmail.com>
2023-03-17 22:12:21 -0700

    another commit

--- app/src/main/java/com/example/southernarizonaapp/ui/SoAzCategoryListScreen.kt
+++ app/src/main/java/com/example/southernarizonaapp/ui/SoAzCategoryListScreen.kt
@@ -1,10 +1,13 @@
 package com.example.southernarizonaapp.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.southernarizonaapp.data.Category
@@ -29,6 +32,10 @@ fun CategoryListItem(
     modifier: Modifier = Modifier
 ) {
     Card(modifier = modifier) {
+        Row {
+            Image(painter =, contentDescription =)
+            Text(text = category.displayName)
+        }
 
     }
 }
--- app/src/main/java/com/example/southernarizonaapp/ui/SoAzUiState.kt
+++ app/src/main/java/com/example/southernarizonaapp/ui/SoAzUiState.kt
@@ -7,5 +7,7 @@ data class SoAzUiState(
     val currentCategory: Category,
     val currentRecommendation: Recommendation,
     val isShowingCategoryScreen: Boolean,
-
-)
+    val categories: Map<Category, List<Recommendation>>,
+) {
+    val currentRecommendations: List<Recommendation> by lazy {categories[currentCategory]!!}
+}
