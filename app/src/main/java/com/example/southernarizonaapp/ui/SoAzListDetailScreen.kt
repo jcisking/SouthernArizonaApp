fe490e626ce5535932349ed0a6e3e5ad6fb61667
jcisking <krisloeffler@gmail.com>
2023-03-19 23:57:56 -0700

    another commit

--- app/src/main/java/com/example/southernarizonaapp/ui/SoAzListDetailScreen.kt
+++ app/src/main/java/com/example/southernarizonaapp/ui/SoAzListDetailScreen.kt
@@ -1,26 +1,62 @@
 package com.example.southernarizonaapp.ui
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.NavigationRail
+import androidx.compose.material3.PermanentNavigationDrawer
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SoAzListDetailScreen(
     windowSize: WindowWidthSizeClass,
     uiState: SoAzUiState,
     modifier: Modifier = Modifier
 ) {
-
-    when(windowSize) {
-
+    when (windowSize) {
         WindowWidthSizeClass.Expanded -> {
-            SoAzListAndDetailScreen()
+            PermanentNavigationDrawer(drawerContent = { /*TODO*/ }) {
+                if (uiState.currentCategory == null) {
+                    SoAzPlaceHolderScreen()
+                }
+                else SoAzListAndDetailScreen()
+                
+            }
         }
 
-        WindowWidthSizeClass.Compact, WindowWidthSizeClass.Medium -> {
+        WindowWidthSizeClass.Medium -> {
+            Row {
+                NavigationRail() {
 
+                }
+                if (uiState.currentCategory == null) {
+                    SoAzPlaceHolderScreen()
+                }
+                else if (uiState.isShowingRecommendationScreen) {
+                    SoAzRecommendationListScreen(
+                        recommendations = /*TODO*/,
+                        onBackButtonClicked = { /*TODO*/ },
+                        uiState = /*TODO*/
+                    )
+                }
+                else SoAzDetailScreen(recommendation = )
+            }
+        }
+        
+        WindowWidthSizeClass.Compact -> {
+            if (uiState.currentCategory == null) {
+                SoAzPlaceHolderScreen()
+            }
+            else if (uiState.isShowingRecommendationScreen) {
+                SoAzRecommendationListScreen(
+                    recommendations = ,
+                    onBackButtonClicked = { /*TODO*/ },
+                    uiState = 
+                )
+            }
+            else SoAzDetailScreen(recommendation = )
         }
     }
-
-    SoAzCategoryListScreen(categories = uiState.categories.keys.toList())
 }
