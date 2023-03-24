eb20a7346f254b7b6f27dd04fc77f1ccc4b88555
jcisking <krisloeffler@gmail.com>
2023-03-23 22:19:07 -0700

    added navigation items

--- app/src/main/java/com/example/southernarizonaapp/ui/SoAzListDetailScreen.kt
+++ app/src/main/java/com/example/southernarizonaapp/ui/SoAzListDetailScreen.kt
@@ -1,15 +1,16 @@
 package com.example.southernarizonaapp.ui
 
+import androidx.annotation.DrawableRes
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import com.example.southernarizonaapp.data.Category
 import com.example.southernarizonaapp.ui.utils.SoAzContentType
 import com.example.southernarizonaapp.ui.utils.SoAzNavigationType
+import com.example.southernarizonaapp.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -20,13 +21,35 @@ fun SoAzListDetailScreen(
     modifier: Modifier = Modifier
 ) {
     data class NavigationItemContent(
-        val icon: ImageVector,
-        val text: String,
+        @DrawableRes val icon: Int,
+        val text: Int,
         val category: Category
     ) {
         val navigationItemContentList: List<NavigationItemContent> = listOf(
             NavigationItemContent(
-
+                icon = R.drawable.restaurant_fill0_wght400_grad0_opsz48,
+                text = R.string.nav_item_restaurant_text,
+                category = Category.RESTAURANTS
+            ),
+            NavigationItemContent(
+                icon = R.drawable.attractions_fill0_wght400_grad0_opsz48,
+                text = R.string.nav_item_tourist_attractions_text,
+                category = Category.TOURIST_ATTRACTIONS
+            ),
+            NavigationItemContent(
+                icon = R.drawable.shopping_cart_fill0_wght400_grad0_opsz48,
+                text = R.string.nav_item_shopping_text,
+                category = Category.SHOPPING
+            ),
+            NavigationItemContent(
+                icon = R.drawable.nature_people_fill0_wght400_grad0_opsz48,
+                text = R.string.nav_item_parks_text,
+                category = Category.PARKS
+            ),
+            NavigationItemContent(
+                icon = R.drawable.hotel_fill0_wght400_grad0_opsz48,
+                text = R.string.nav_item_resorts_text,
+                category = Category.RESORTS
             )
         )
 
@@ -70,6 +93,7 @@ fun SoAzContent(
     Row {
         AnimatedVisibility(visible = navigationType == SoAzNavigationType.NAVIGATION_RAIL) {
             NavigationRail() {
+                
                 
             }
         }
