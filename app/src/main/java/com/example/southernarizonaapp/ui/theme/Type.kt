fe0c6fab4cd9f79fa370a92f9a3e57ac345e7013
jcisking <krisloeffler@gmail.com>
2023-03-28 21:34:24 -0700

    added typography

--- app/src/main/java/com/example/southernarizonaapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/southernarizonaapp/ui/theme/Type.kt
@@ -2,16 +2,43 @@ package com.example.southernarizonaapp.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.southernarizonaapp.R
+
+
+val Barlow = FontFamily(
+    Font(R.font.barlow_extra_light, FontWeight.ExtraLight),
+    Font(R.font.barlow_medium, FontWeight.Medium)
+)
+
+val BreeSerif = FontFamily(
+    Font(R.font.bree_serif_regular, FontWeight.Normal)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+    h1 = TextStyle(
+        fontFamily = BreeSerif,
         fontWeight = FontWeight.Normal,
+        fontSize = 20.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = BreeSerif,
+        fontWeight = FontWeight.Normal,
+        fontSize = 15.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = Barlow,
+        fontWeight = FontWeight.Medium,
         fontSize = 16.sp
+    ),
+    button = TextStyle(
+        fontFamily = Barlow,
+        fontWeight = FontWeight.ExtraLight,
+        fontSize = 10.sp
     )
     /* Other default text styles to override
     button = TextStyle(
